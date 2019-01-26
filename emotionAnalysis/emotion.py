e48a37dc7eb3ef8176d22800db6ad335a6b8d496
예진 서 <yejinseo@dyn3182-129.wlan.ic.ac.uk>
2019-01-26 18:19:24 +0000

    emotion analysable, need to make it parse through our own pictures

--- emotionAnalysis/emotion.py
+++ emotionAnalysis/emotion.py
@@ -1,7 +1,5 @@
 import http.client, urllib.request, urllib.parse, urllib.error, base64
-import pathlib
-
-facephoto = pathlib.Path("/Users/yejinseo/ichack19/faceStalker/laugh.jpg").as_uri()
+import json
 
 headers = {
     # Request headers
@@ -10,15 +8,44 @@ headers = {
 }
 
 params = urllib.parse.urlencode({
-    "returnFaceAttributes": {"emotion"}
+    # Request parameters
+    'returnFaceId': 'true',
+    'returnFaceLandmarks': 'false',
+    'returnFaceAttributes': "emotion",
 })
-
+body = json.dumps({"url": "http://wp.doc.ic.ac.uk/ajf/wp-content/uploads/sites/49/2014/01/meBigHoleAdjusted.jpg"})
 try:
     conn = http.client.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
-    conn.request("POST", "/face/v1.0/detect?%s" % params, "http://wp.doc.ic.ac.uk/ajf/wp-content/uploads/sites/49/2014/01/meBigHoleAdjusted.jpg", headers)
+    conn.request("POST", "/face/v1.0/detect?%s" % params, body, headers)
     response = conn.getresponse()
     data = response.read()
     print(data)
     conn.close()
 except Exception as e:
     print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+
+# import http.client, urllib.request, urllib.parse, urllib.error, base64
+# import pathlib
+#
+# facephoto = pathlib.Path("/Users/yejinseo/ichack19/faceStalker/laugh.jpg").as_uri()
+#
+# headers = {
+#     # Request headers
+#     'Content-Type': 'application/json',
+#     'Ocp-Apim-Subscription-Key': 'c91dcc9052a04898890b376f8a526c4f',
+# }
+#
+# params = urllib.parse.urlencode({
+#     "returnFaceAttributes": {"emotion"}
+# })
+#
+# try:
+#     conn = http.client.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
+#     conn.request("POST", "/face/v1.0/detect?%s" % params, "http://wp.doc.ic.ac.uk/ajf/wp-content/uploads/sites/49/2014/01/meBigHoleAdjusted.jpg", headers)
+#     response = conn.getresponse()
+#     data = response.read()
+#     print(data)
+#     conn.close()
+# except Exception as e:
+#     print("[Errno {0}] {1}".format(e.errno, e.strerror))
