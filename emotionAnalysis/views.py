54d967ef4048374de1aa86b8bebe1bea8de7fa0a
예진 서 <yejinseo@dyn3182-129.wlan.ic.ac.uk>
2019-01-27 00:56:48 +0000

    sub to the backend done

--- emotionAnalysis/views.py
+++ emotionAnalysis/views.py
@@ -2,22 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from . import emotion, take_photo
 import statistics
+import os
+from google.cloud import pubsub_v1
 
-# Create your views here.
-
-# Score of each emotion
-emotion_scores = {
-    "anger": -7,
-    "contempt": -5,
-    "disgust": -10,
-    "fear": -4,
-    "happiness": 10,
-    "neutral": 0,
-    "sadness": -2,
-    "surprise": 6
-}
-
-def analysis(request):
+def callback(request):
     filenames = []
 
     scores = []
@@ -47,3 +35,35 @@ def analysis(request):
 
     # Response soth the emotion score of the person
     return HttpResponse(median)
+
+
+subscriber = pubsub_v1.SubscriberClient()
+topic_name = 'projects/{project_id}/topics/{topic}'.format(
+    project_id='huddle72',
+    topic='vote',  # Set this to something appropriate.
+)
+subscription_name = 'projects/{project_id}/subscriptions/{sub}'.format(
+    project_id='huddle72',
+    sub='machine3',  # Set this to something appropriate.
+)
+
+
+# def callback(message):
+#     print(message.data)
+#     message.ack()
+
+future = subscriber.subscribe(subscription_name, callback)
+
+# Create your views here.
+
+# Score of each emotion
+emotion_scores = {
+    "anger": -7,
+    "contempt": -5,
+    "disgust": -10,
+    "fear": -4,
+    "happiness": 10,
+    "neutral": 0,
+    "sadness": -2,
+    "surprise": 6
+}
